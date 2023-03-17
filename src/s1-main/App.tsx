eec866e78ab6a5cd05bb5e215b0b9b61640fdd07
Natikana <mashkovski@gmail.com>
2023-03-17 12:53:40 +0100

    add HW11

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import s from './App.module.css'
 import HW1 from '../s2-homeworks/hw01/HW1'
+import HW11 from "../s2-homeworks/hw11/HW11";
 
 function App() {
     return (
         <div className={s.App}>
-            <HW1/>
+            <HW11/>
             {/*раскомментировать по ходу выполнения*/}
             {/*<HW2/>*/}
             {/*<HW3/>*/}
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import {capitalize, Slider, SliderProps} from '@mui/material'
+
+const CustomSliderStyles = {
+    '& .MuiSlider-thumb': {
+        color: "green"
+    },
+    '& .MuiSlider-track': {
+        color: "green"
+    },
+    '& .MuiSlider-rail': {
+        color: "grey"
+    },
+    '& .MuiSlider-active': {
+        color: "lightGreen"
+    },
+};
+
+
 
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
+            sx={ // стили для слайдера // пишет студент
+                CustomSliderStyles}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
     )
