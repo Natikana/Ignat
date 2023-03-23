a274f906a0f548dd0bc636487901b9d66f60273b
Natikana <mashkovski@gmail.com>
2023-03-23 10:04:58 +0100

    add correct route

---
+++ src/s2-homeworks/hw4/common/c2-SuperButton/SuperButton.tsx
@@ -0,0 +1,32 @@
+import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react'
+import s from './SuperButton.module.css'
+
+// тип пропсов обычной кнопки, children в котором храниться название кнопки там уже описан
+type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+
+type SuperButtonPropsType = DefaultButtonPropsType & {
+    red?: boolean
+}
+
+const SuperButton: React.FC<SuperButtonPropsType> = (
+    {
+        red, className,
+        ...restProps// все остальные пропсы попадут в объект restProps, там же будет children
+    }
+) => {
+    const finalClassName = `${red ? s.red : s.default} ${className}`
+
+    return (
+        <button
+            style={{background: 'white',
+                color: '#147cb9',
+                width: '150px',
+                border: '#147cb9 2px solid',
+                height: '25px'}}
+            className={finalClassName}
+            {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
+        />
+    )
+}
+
+export default SuperButton
---
+++ src/s2-homeworks/hw5/pages/Junior.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import HW7 from "../../hw7/HW7";
+import HW8 from "../../hw8/HW8";
+import HW9 from "../../hw9/HW9";
+import HW10 from "../../hw10/HW10";
+import HW6 from "../../hw6/HW6";
+
+function Junior() {
+    return (
+        <div>
+            <HW6/>
+            <HW7/>
+            <HW8/>
+            <HW9/>
+            <HW10/>
+        </div>
+    )
+}
+
+export default Junior
